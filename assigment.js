3be61deef4f1ec2c3897f17c053140b58deddef4
akankshalata1234-boop <akankshalata1234@gmail.com>
2026-02-04 10:53:25 +0530

    add example

--- assigment.js
+++ assigment.js
@@ -3,3 +3,7 @@ let b;
 console.log("before using assigment operator a=",a);
 console.log("assigment operator operate on two variable");
 console.log("after using assigment operator b=",b=a);
+//here using assigment operator another 
+let y=5;
+ y+=2;
+console.log("print the number",y);
