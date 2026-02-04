6756dea4a5cb144b7fb43be347a83ab58d6d0bfb
akankshalata1234-boop <akankshalata1234@gmail.com>
2026-02-04 10:36:30 +0530

    adding assigment operator

---
+++ assigment.js
@@ -0,0 +1,5 @@
+let a=3;
+let b;
+console.log("before using assigment operator a=",a);
+console.log("assigment operator operate on two variable");
+console.log("after using assigment operator b=",b=a);
